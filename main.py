e6872c40c282f81668c92168d74f23af5c358879
martipops <melonnemann@gmail.com>
2024-04-21 23:06:11 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def animate_ball():
+    position = 0
+    velocity = 1
+    gravity = 0.1
+
+    while True:
+        clear_screen()
+
+        position += velocity
+        velocity -= gravity
+
+        if position <= 0:
+            position = 0
+            velocity = -velocity * 0.9
+
+        print("\n" * int(position) + "o")
+
+        time.sleep(0.1)
+
+animate_ball()
